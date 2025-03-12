d5345fee7c9bc0fc8656e63443d952e6f7c93802
yuliussmayoru <yuliussmayoru@gmail.com>
2025-03-12 22:49:05 +0700

    update

---
+++ src/api/axiosClient.ts
@@ -0,0 +1,33 @@
+import { token } from "@/config";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const axiosInstance = axios.create({
+    baseURL : baseUrl,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+console.log(axiosInstance)
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const auth = Cookies.get(token);
+        // const token = localStorage.getItem('authToken');
+
+        if (auth) { config.headers.Authorization = `Bearer ${auth}`;
+        console.log('Token is saved:', auth);
+        }  else {
+            console.warn("token not found")
+        } 
+        return config;
+
+    },
+    (error) => {
+        console.error("Request error:", error);
+        return Promise.reject(error);
+    }
+)

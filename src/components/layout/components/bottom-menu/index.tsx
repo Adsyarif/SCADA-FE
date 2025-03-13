d3fa11811307e80a04e6adc9a5bf1ec9af063299
yuliussmayoru <yuliussmayoru@gmail.com>
2025-03-14 04:21:46 +0700

    feat: updating homepage layout

--- src/components/layout/components/bottom-menu/index.tsx
+++ src/components/layout/components/bottom-menu/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function BottomMenu() {
     return (
-        <div className="flex space-between gap-4">
+        <div className="flex justify-around gap-4 p-4">
             <Link href="/"><HouseIcon /></Link>
             <Link href="/"><Mail /></Link>
             <Link href="/"><Bell/></Link>

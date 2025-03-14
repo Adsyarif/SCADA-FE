6d3c4e22342458b6acfd14966ac248e72bac9bd7
adrisa <adsyarif28@gmail.com>
2025-03-14 13:39:09 +0700

    feat/fixing-outer-layout-non-mobile

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="antialiased">
+      <body className="antialiased flex h-screen w-full justify-center items-center bg-gray-200">
         <Main />
         <NextScript />
       </body>

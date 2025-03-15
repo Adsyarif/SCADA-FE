51c9e8eac64e65b5855eaa08d32816fc3a310396
yuliussmayoru <yuliussmayoru@gmail.com>
2025-03-15 18:39:58 +0700

    feat: finishing ui homepage-2

--- src/components/menu-items/index.tsx
+++ src/components/menu-items/index.tsx
@@ -4,8 +4,8 @@ import { MenuItemsProps } from "./type";
 export function MenuItems(props: MenuItemsProps) {
     const { href, children, menuName} = props
     return(
-        <Link href={href}>
-            <div>{children}</div><span>{menuName}</span>
+        <Link href={href} className="flex flex-col text-centerjustify-center items-center">
+            <div className="p-2">{children}</div><span className={"text-center"}>{menuName}</span>
         </Link>
     )
 }

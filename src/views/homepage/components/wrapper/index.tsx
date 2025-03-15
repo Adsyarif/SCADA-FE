46d36da7e8b26d5aee751832d927d059ee2b8e1f
yuliussmayoru <yuliussmayoru@gmail.com>
2025-03-15 18:39:58 +0700

    feat: finishing ui homepage-2

--- src/views/homepage/components/wrapper/index.tsx
+++ src/views/homepage/components/wrapper/index.tsx
@@ -5,22 +5,26 @@ import Link from "next/link";
 export function HomepageWrapper () {
     return (
         <div className="flex flex-col">
-            <div className="grow">
-                <div className="flex justify-between items-center w-full p-8">
-                    <div className="w-1/3 flex justify-center">
-                        <div className="rounded-full bg-slate-200 w-16 h-16 flex justify-center items-center"><User className="w-12 h-12"/></div>
-                    </div>
+            <div className="grow flex flex-col">
+                <div className="grow flex justify-between items-center w-full px-8">
                     <div>
-                        <p className="line-clamp-2 text-justify">
-                            here written what should be written
+                        <div className="flex justify-center">
+                            <div className="rounded-full bg-slate-200 w-24 h-24 flex justify-center items-center"><User className="w-16 h-16"/></div>
+                        </div>
+                    </div>
+                    <div className={"p-4 flex justify-center"}>
+                        <p className="line-clamp-4 text-justify">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo nisi, blandit vel est et, pulvinar molestie dui. Quisque rutrum purus et ex ornare, ut blandit justo fermentum. Curabitur eu dapibus elit, eget faucibus neque. Donec pharetra mattis urna ac facilisis. Suspendisse gravida lectus orci, id blandit quam viverra a. Fusce quis diam erat. Vestibulum et malesuada nibh. Integer viverra tempus ullamcorper. Vestibulum quis fringilla ligula, et volutpat velit.
                         </p>
                     </div>
                 </div>
-                <div>
-                    <div className="flex justify-around my-4 items-end">
-                        <h2 className="text-xl font-black">Status RTU</h2> <Link href="/" className="text-blue-800 text-xs">Lainnya...</Link>
+                <div className="grow flex flex-col justify-center">
+                    <div className="grow flex justify-center my-4 items-center">
+                        <div className={"w-full flex justify-around items-center"}>
+                            <h2 className="text-xl font-black">Status RTU</h2> <Link href="/" className="text-blue-800 text-xs">Lainnya...</Link>
+                        </div>
                     </div>
-                    <div className="grid grid-cols-3 justify-items-center content-center">
+                    <div className="grow grid grid-cols-3 justify-items-center content-center">
                         <Indicator value={2000} indicatorName={"Indikator 1"}/>
                         <Indicator value={1500} indicatorName={"Indikator 2"}/>
                         <Indicator value={1000} indicatorName={"Indikator 3"}/>
@@ -30,10 +34,10 @@ export function HomepageWrapper () {
             <div className="grow bg-gray-200">
                 <div className="grid grid-cols-4 gap-4 text-xs justify-items-center align-center p-8">
                     <MenuItems href="/" menuName="Lihat Jadwal"><Calendar1 /></MenuItems>
-                    <Link href="/"><CalendarCheck />Absensi</Link>
-                    <Link href="/"><File />Buat Laporan</Link>
-                    <Link href="/"><UserSquare2 />Daftar Operator</Link>
-                    <Link href="/"><Wallet />Laporan Keuangan</Link>
+                    <MenuItems href="/" menuName="Absensi"><CalendarCheck /></MenuItems>
+                    <MenuItems href="/" menuName="Buat Laporan"><File /></MenuItems>
+                    <MenuItems href="/" menuName="Daftar Operator"><UserSquare2 /></MenuItems>
+                    <MenuItems href="/" menuName="Laporan Keurangan"><Wallet /></MenuItems>
                 </div>
             </div>
         </div>

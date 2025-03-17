56b3cf60e7976d61abcf157b5e25f71201ee403a
yuliussmayoru <yuliussmayoru@gmail.com>
2025-03-17 14:38:44 +0700

    feat:update view absensi

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="en">
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+                    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+                    crossOrigin="" />
         </Head>
         <body className="antialiased flex h-screen w-full justify-center items-center bg-gray-200">
         <Main/>

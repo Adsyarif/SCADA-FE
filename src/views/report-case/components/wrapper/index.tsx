4058a7ef39b51aba80fbaa5843fb98c4dbb76c5e
adrisa <adsyarif28@gmail.com>
2025-05-03 20:52:38 +0700

    feat/integrated category report api

--- src/views/report-case/components/wrapper/index.tsx
+++ src/views/report-case/components/wrapper/index.tsx
@@ -2,78 +2,102 @@ import { Button, Dropdown } from "@/components";
 import { ImgUpload } from "@/components/base/img-upload-btn";
 import { XIcon } from "lucide-react";
 import { useState } from "react";
+import { useAsignedSupervisor, useReportCategory } from "../../hooks";
+import { useSession } from "next-auth/react";
 
 export function ReportCase() {
-    const [selectedValue, setSelectedValue] = useState("")
-    const [fileName, setFileName] = useState("")
-    const options = ['Option A', 'Option B', 'Option C'];
+  const { data: session } = useSession();
+  const staffId = session?.user?.id;
+  const [selectedValue, setSelectedValue] = useState("");
+  const [fileName, setFileName] = useState("");
 
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.files && event.target.files.length > 0 ) {
-            setFileName(event.target.files[0].name)
-        }
+  const { data: categories, isLoading, error } = useReportCategory();
+  const reportCategories = categories?.data;
+  console.log(staffId);
+  const {
+    data: supervisor,
+    isLoading: superVisorLoading,
+    error: supervisorError,
+  } = useAsignedSupervisor(staffId!);
 
-        if (!event.target.files?.[0]) return
-        const file = event.target.files[0];
-        
-        const reader = new FileReader();
-        reader.onload = () => {
-            if (typeof reader.result === 'string') {
-            console.log('File content: ', reader.result);
-            }
-        };
-        reader.readAsDataURL(file);
+  console.log(supervisor);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  const options = reportCategories!?.map((cat) => cat.categoryName);
+  console.log(options);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setFileName(event.target.files[0].name);
     }
-    return (
-        <div className="flex flex-col w-full pt-16">
-            <form id={"action"} className={"grow"}>
-                <div className="w-full flex justify-center">
-                    <h1 className={"text-2xl font-semibold p-4"}>Buat Laporan</h1>
-                </div>
-                <div className={"flex flex-col justify-start items-start gap-4 m-4"}>
-                    <div className="flex flex-col">
-                        <span>Kepada:</span> 
-                        <span>{"Bpk. Bambang"}</span> {/*get user supervisor */}
-                    </div>
-                    <div>
-                        <Dropdown
-                            options={options}
-                            value={selectedValue}
-                            onValueChange={(val) => setSelectedValue(val)}
-                        />
-                    </div>
-                </div>
-                <div className="flex flex-col p-4">
-                    <textarea
-                        name="text-area"
-                        id="text-area"
-                        placeholder="Tulis Laporan Kamu disini"
-                        className="border border-black rounded-lg p-4 w-full h-96"
-                    />
-                    <div>
-                        <div className="flex gap-2 py-2">
-                            {fileName && (
-                                <div className="flex gap-2 w-32">
-                                    <p className="truncate">{fileName}</p>
-                                    <button
-                                        onClick={()=> setFileName("")}
-                                    ><XIcon className="cursor-pointer"/></button>
-                                </div>
-                            )}
-                            
-                            <ImgUpload
-                                onChange={handleFileChange}
-                                accept=".jpg, .png"
-                                multiple={true}
-                            />
-                        </div>
-                    </div>
-                </div>
-                <div className="flex gap-4 p-4">
-                    <Button>Kirim</Button>
-                    <Button containerClassName="bg-white border text-black">Batal</Button>
-                </div>
-            </form>
+
+    if (!event.target.files?.[0]) return;
+    const file = event.target.files[0];
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (typeof reader.result === "string") {
+        console.log("File content: ", reader.result);
+      }
+    };
+    reader.readAsDataURL(file);
+  };
+  return (
+    <div className="flex flex-col w-full pt-16">
+      <form id={"action"} className={"grow"}>
+        <div className="w-full flex justify-center">
+          <h1 className={"text-2xl font-semibold p-4"}>Buat Laporan</h1>
         </div>
-    )
+        <div className={"flex flex-col justify-start items-start gap-4 m-4"}>
+          <div className="flex flex-col">
+            <span>Kepada:</span>
+            <span>{"Bpk. Bambang"}</span> {/*get user supervisor */}
+          </div>
+          <div>
+            <Dropdown
+              options={options}
+              value={selectedValue}
+              onValueChange={(val) => setSelectedValue(val)}
+            />
+          </div>
+          <div>
+            {reportCategories?.map((category) => (
+              <div key={category.categoryId}>{category.categoryName}</div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col p-4">
+          <textarea
+            name="text-area"
+            id="text-area"
+            placeholder="Tulis Laporan Kamu disini"
+            className="border border-black rounded-lg p-4 w-full h-96"
+          />
+          <div>
+            <div className="flex gap-2 py-2">
+              {fileName && (
+                <div className="flex gap-2 w-32">
+                  <p className="truncate">{fileName}</p>
+                  <button onClick={() => setFileName("")}>
+                    <XIcon className="cursor-pointer" />
+                  </button>
+                </div>
+              )}
+
+              <ImgUpload
+                onChange={handleFileChange}
+                accept=".jpg, .png"
+                multiple={true}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-4 p-4">
+          <Button>Kirim</Button>
+          <Button containerClassName="bg-white border text-black">Batal</Button>
+        </div>
+      </form>
+    </div>
+  );
 }

0ad30a5fa5ce9aa920cea4b35a0446334c5f2828
Adrisa <adrisansyarif@gmail.com>
2025-05-25 23:59:57 +0700

    feat: add operator list until get detail reports relevant to supervisor

--- src/views/log-report/components/wrapper/index.tsx
+++ src/views/log-report/components/wrapper/index.tsx
@@ -1,44 +1,50 @@
-import { ListDateItem, Title } from "@/components";
+import { Title } from "@/components";
 import { UserNameInterface } from "@/context";
+import ReportList from "@/views/report-menu/components/ReportList";
 import { useListReport } from "@/views/report-menu/hooks";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 const LogReportWrapper = ({ userId, userName }: UserNameInterface) => {
-  const { data: reports, error, isLoading } = useListReport(userId || "");
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
+
+  const route = useRouter();
+  const { data: reports, error, isLoading } = useListReport(userId);
+  const reportsData = reports?.data;
+  console.log(reportsData);
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading reports</p>;
+  const handleBackBtn = () => {
+    route.push("/list-operator");
+  };
+  let data: any = [];
+  if (Array.isArray(reportsData)) {
+    data = reportsData;
+  }
+  const totalPages = Math.ceil(data.length || 0 / itemsPerPage);
+  const paginatedReports = data.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
 
   return (
     <div className="flex grow">
-      <div className="grow w-full bg-whte">
-        <Title text={"Log Report"} isButton={true} />
+      <div className="grow w-full">
+        <Title
+          text={"Log Report"}
+          isButton={true}
+          handleBackClick={() => handleBackBtn()}
+        />
         <div className="h-14 flex items-center justify-center">
           <h1 className="font-bold text-xl">{userName}</h1>
         </div>
         <div className="">
-          <ListDateItem
-            status={"approved"}
-            page={"log-report"}
-            reportName={"Report Kategori"}
-            reportDesc={
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, itaque sit optio earum vitae atque blanditiis enim perferendis laudantium sequi animi ratione minima aspernatur est eaque. Cupiditate asperiores nulla laboriosam?"
-            }
-            onClick={() => {}}
-          />
-          <ListDateItem
-            status={"needRevision"}
-            page={"log-report"}
-            reportName={"Report Kategori"}
-            reportDesc={
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, itaque sit optio earum vitae atque blanditiis enim perferendis laudantium sequi animi ratione minima aspernatur est eaque. Cupiditate asperiores nulla laboriosam?"
-            }
-            onClick={() => {}}
-          />
-          <ListDateItem
-            status={"review"}
-            page={"log-report"}
-            reportName={"Report Kategori"}
-            reportDesc={
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, itaque sit optio earum vitae atque blanditiis enim perferendis laudantium sequi animi ratione minima aspernatur est eaque. Cupiditate asperiores nulla laboriosam?"
-            }
-            onClick={() => {}}
+          <ReportList
+            reports={paginatedReports}
+            currentPage={currentPage}
+            totalPages={totalPages}
+            setCurrentPage={setCurrentPage}
           />
         </div>
       </div>

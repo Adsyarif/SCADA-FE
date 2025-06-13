63b143b44bbf57011c3f53d98c886c94fb719c77
yuliussmayoru <yuliussmayoru@gmail.com>
2025-06-13 22:08:33 +0700

    fix: fixing update RTU configuration

--- src/views/rtu-configuration/api/index.ts
+++ src/views/rtu-configuration/api/index.ts
@@ -1,6 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axiosInstance from "@/api/axiosClient";
 import type { RtuConfiguration, PaginatedRtus } from "../types";
+import { AxiosResponse } from "axios";
+import { RtuFormData } from "../schema";
+import { useRouter } from "next/router";
 
 export function useRtuConfigurations() {
   return useQuery<RtuConfiguration[], Error>({
@@ -12,4 +15,31 @@ export function useRtuConfigurations() {
         })
         .then((res) => res.data.data),
   });
+}
+
+export function useCreateRTU() {
+  const qc = useQueryClient()
+  const router = useRouter()
+
+  return useMutation<AxiosResponse<any>, Error, RtuFormData>({
+    mutationFn: data => axiosInstance.post('/rtu-configuration', data),
+    onSuccess: () => {
+      qc.invalidateQueries({ queryKey: ['rtus'] })
+      router.push('/rtu-configuration')
+    }
+  })
+}
+
+export function useUpdateRtuConfiguration(id: string) {
+  const qc = useQueryClient()
+  const router = useRouter()
+
+  return useMutation({
+    mutationFn: (data: RtuFormData) =>
+      axiosInstance.patch(`/rtu-configuration/${id}`, data),
+    onSuccess: () => {
+      qc.invalidateQueries({ queryKey: ['rtus'] })
+      router.push('/rtu-configuration')
+    }
+  })
 }

d9fdc66e12c3a991eb7a5a2c7a207abaaa94e1e1
adrisa <adsyarif28@gmail.com>
2025-07-24 00:02:39 +0700

    fixedTemp: CalendarSchedule component need shiftId props

--- src/views/operator-schedule/components/wrapper/index.tsx
+++ src/views/operator-schedule/components/wrapper/index.tsx
@@ -1,14 +1,13 @@
 import { Title } from "@/components";
 import { CalendarSchedule } from "@/views";
 
-
 export function OperatorScheduleWrapper() {
-    return (
-        <div className="flex flex-col w-full">
-            <Title isButton backHref="/homepage" text="Calendar"/>
-            <div>
-                <CalendarSchedule />
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col w-full">
+      <Title isButton backHref="/homepage" text="Calendar" />
+      <div>
+        <CalendarSchedule shiftId={""} />
+      </div>
+    </div>
+  );
 }
